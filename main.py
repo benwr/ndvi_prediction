2b723c03b2d3639708adff6f86fd3f7b251d54e2
Ben Weinstein-Raun <bwr@vt.edu>
2013-10-28 16:07:56 -0400

    switched to using only one point in the history rather than the surrounding square.

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from pybrain.datasets import SupervisedDataSet
 def main():
 
   # Input consists of the NDVI of the square surrounding a point, and the point 
-  #   itself, over the prior year's worth of observations (9 data points times 
-  #   3 data sets times twelve months), the value of the current month in hours, 
+  #   itself, over the prior year's worth of observations (1 data point times 3
+  #   data sets times twelve months), the value of the current month in hours, 
   #   and the latitude and longitude of the point.
 
   # Output is the NDVI of the point.
-  ds = SupervisedDataSet( ((9*3)*12) + 1 + 2, 1 )
+  ds = SupervisedDataSet( 3*12 + 1 + 2, 1 )
 
   veg_data = get_ncdf_data('ndviavhrr19812001.nc')
   prec_data = get_ncdf_data('precipud19002008.nc')
@@ -31,13 +31,13 @@ def main():
     for month in range(0, 12):
       t = int(12*year + month)
       for lat in range(10, lats):
+        print lat
         for lon in range(lons):
           s = is_available_point(veg_data, prec_data, temp_data, lat, lon, t, offset)
           if s != None:
             print t, lat, lon
             ds.addSample((list(s.ravel()) + [t % 12, lat, lon]), 
                           (veg_data[t,lat,lon],))
-        print "nextlat"
   return ds
 
 
@@ -55,21 +55,12 @@ def is_available_point(ndvi, prec, temp, lat, lon, t, offset):
   lat = int(lat)
   lon = int(lon)
   offset = int(offset)
-  result = [ndvi[range(t-12, t), 
-    [lat-1, lat, lat+1], 
-    [lon-1, lon, lon+1]],
-     
-    prec[range(t-12+offset, t+offset), 
-      [(lat-1)*2, lat*2, (lat+1)*2], 
-      [(lon-1)*2, lon*2, (lon+1)*2]],
-    
-    temp[range(t-12+offset, t+offset), 
-      [(lat-1)*2, lat*2, (lat+1)*2], 
-      [(lon-1)*2, lon*2, (lon+1)*2]]]
+  result = [ndvi[range(t-12, t), lat, lon],
+    prec[range(t-12+offset, t+offset), lat*2, lon*2],
+    temp[range(t-12+offset, t+offset), lat*2, lon*2]]
 
   for set in result:
-    s = set.shape
-    if type(set) != np.ndarray or s[0] != 12 or s[1] != 3 or s[2] != 3:
+    if type(set) != np.ndarray or set.shape[0] != 12:
       return None
 
   return np.array(result)
